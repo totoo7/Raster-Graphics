c927755ea588053d35917263d4dc09de9658bc3e
totoo7 <stoyanorozov@gmail.com>
2025-06-03 08:27:25 +0300

    Small redo fix

--- src/services/commands/RedoCommand.cpp
+++ src/services/commands/RedoCommand.cpp
@@ -4,6 +4,7 @@ RedoCommand::RedoCommand(Session* const session)
 	: session(session) {}
 
 std::string RedoCommand::execute() {
+	if (session->redos.empty()) return "Nothing to redo.\n";
 	session->redo();
 	return "Redo was successful.\n";
 }
